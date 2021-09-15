ac08f0df7a98c2fb8f49426c0b890b9c7bd2b349
Siehan <memain.ye.sylvie@gmail.com>
2021-09-15 20:04:49 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import "./App.css";
 import * as React from "react";
 import LandingPage from "./components/LandingPage";
 
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from "@chakra-ui/react";
-
 function App() {
   // 2. Use at the root of your app
   return (
-    <ChakraProvider>
-      <LandingPage />
-    </ChakraProvider>
+    <div className="App">
+      <header>
+        <LandingPage />
+      </header>
+    </div>
   );
 }
 
